0d8d89af0931a4eb42b27e3a630e2a665005100b
jupablota <jupablota2005@gmail.com>
2025-04-29 15:23:23 -0500

    perdob

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,22 +1,37 @@
 import pandas as pd
+
 import aiohttp
+
+import asyncio
+
 import io
 
-CSV_URL = "https://ourworldindata.org/grapher/crude-birth-rate.csv?v=1&csvType=full&useColumnShortNames=false"
-DATAFRAME = pd.DataFrame()
 
 async def fetch_csv():
-    global DATAFRAME
+
+    CSV_URL = "https://ourworldindata.org/grapher/crude-birth-rate.csv?v=1&csvType=full&useColumnShortNames=false"
+
     async with aiohttp.ClientSession() as session:
+
         async with session.get(CSV_URL) as resp:
+
             if resp.status == 200:
+
                 content = await resp.read()
-                print(f"Contenido descargado, tamaño: {len(content)} bytes")  # Imprime el tamaño del contenido descargado
+
                 try:
+
                     df = pd.read_csv(io.BytesIO(content))
-                    DATAFRAME = df
-                    print(f"Datos cargados correctamente, tamaño del DataFrame: {len(DATAFRAME)}")  # Imprime el tamaño del DataFrame
+
+                    print(f"Data loaded successfully, DataFrame size: {len(df)}")
+
                 except Exception as e:
-                    print(f"Error al procesar los datos CSV: {e}")
+
+                    print(f"Error processing CSV data: {e}")
+
             else:
-                print(f"Error al descargar datos: {resp.status}")
+
+                print(f"Error downloading data: {resp.status}")
+
+
+asyncio.run(fetch_csv())
