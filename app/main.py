6e5b882cff4853b5b2b90e13f122637d58b0eab9
jupablota <jupablota2005@gmail.com>
2025-04-29 14:31:33 -0500

    Hemo vuelto mi gente

--- app/main.py
+++ app/main.py
@@ -2,25 +2,12 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import asyncio
-import pandas as pd
 
 from app.data_loader import fetch_csv, DATAFRAME
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
-CSV_URL = "https://ourworldindata.org/grapher/crude-birth-rate.csv?v=1&csvType=full&useColumnShortNames=false"
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    df = pd.read_csv(CSV_URL)
-    # Tomamos las primeras 100 filas por simplicidad
-    data = df.head(100).to_dict(orient="records")
-    columns = df.columns.tolist()
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "title": "Tasas de Natalidad",
-        "data": data,
-        "columns": columns
-    })
+
 @app.on_event("startup")
 async def startup_event():
     await fetch_csv()
